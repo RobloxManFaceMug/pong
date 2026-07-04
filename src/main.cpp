ac31e7e2c37711e2261d70df3a38c3f5b8d667aa
RobloxManFaceMug <nbahtnxohob@gmail.com>
2026-07-04 18:55:20 +0700

    added screen elements and semi working ball

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,57 @@
 #include "raylib.h"
+#include <iostream>
 
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
+class Ball {
+    public:
+        float x_pos, y_pos;
+        int radius;
+        int vel_x, vel_y;
+
+    void DrawBall() {
+        DrawCircle(x_pos, y_pos, radius, WHITE);
+    }
+
+    void UpdateBall() {
+        x_pos += vel_x;
+        y_pos += vel_y;
+    }
+};
+
+Ball ball;
+
 int main(void)
 {
     // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenWidth = 1280;
+    const int screenHeight = 720;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    InitWindow(screenWidth, screenHeight, "Pong but Awesome");
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+    SetTargetFPS(60);
+
+    ball.x_pos = screenWidth/2;
+    ball.y_pos = screenHeight/2;
+    ball.radius = 20;
+    ball.vel_x = 5;
+    ball.vel_y = 5;
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
         BeginDrawing();
-
-            ClearBackground(RAYWHITE);
-
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-
+        
+        // Drawing The Shapes
+        ball.UpdateBall();
+        ClearBackground(BLACK);
+        ball.DrawBall();
+        DrawRectangle(70, screenHeight/2 - 60, 25, 120, WHITE);
+        DrawRectangle(screenWidth - 95, screenHeight/2 - 60, 25, 120, WHITE);
+        DrawLine(screenWidth/2, 0, screenWidth/2, screenHeight, WHITE);
         EndDrawing();
-        //----------------------------------------------------------------------------------
     }
 
     // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+    CloseWindow();
 
     return 0;
 }
